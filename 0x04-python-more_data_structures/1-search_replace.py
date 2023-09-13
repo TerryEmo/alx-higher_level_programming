1d8aef1c0d8f1dbc97aff33fdc99aabf33c7fef5
TerryEmo <terrance.sabelo@gmail.com>
2023-09-13 11:04:52 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+def search_replace(my_list, search, replace):
+    n_list = list(my_list)
+    for i in range(len(n_list)):
+        if n_list[i] == search:
+            n_list[i] = replace
+    return n_list
