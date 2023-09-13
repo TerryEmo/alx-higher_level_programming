f24ea346aecb0d2294d4da13dd2dd912e136565a
TerryEmo <terrance.sabelo@gmail.com>
2023-09-13 11:21:26 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
     if not my_list:
         return 0
@@ -11,4 +10,4 @@ def weight_average(my_list=[]):
         num += tup[0] * tup[1]
         den += tup[1]
 
-return (num / den)
+    return (num / den)
