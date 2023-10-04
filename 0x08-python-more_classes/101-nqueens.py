fff80ad486c6fa1752708d527bd8d9c8fc675c01
TerryEmo <terrance.sabelo@gmail.com>
2023-10-04 11:15:38 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-
 """Solves the N-queens puzzle.
-
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
-
 Example:
     $ ./101-nqueens.py N
-
 N must be an integer greater than or equal to 4.
-
 Attributes:
     board (list): A list of lists representing the chessboard.
     solutions (list): A list of lists containing solutions.
-
 Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
 where `r` and `c` represent the row and column, respectively, where a
 queen must be placed on the chessboard.
@@ -35,7 +29,6 @@ def board_deepcopy(board):
         return list(map(board_deepcopy, board))
     return (board)
 
-
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
     solution = []
@@ -49,16 +42,14 @@ def get_solution(board):
 
 def xout(board, row, col):
     """X out spots on a chessboard.
-
     All spots where non-attacking queens can no
     longer be played are X-ed out.
-
     Args:
         board (list): The current working chessboard.
         row (int): The row where a queen was last played.
         col (int): The column where a queen was last played.
     """
-    # X out all forward spots
+ # X out all forward spots
     for c in range(col + 1, len(board)):
         board[row][c] = "x"
     # X out all backwards spots
@@ -82,7 +73,7 @@ def xout(board, row, col):
     for r in range(row - 1, -1, -1):
         if c < 0:
             break
-        board[r][c]
+ board[r][c]
         c -= 1
     # X out all spots diagonally up to the right
     c = col + 1
@@ -102,7 +93,6 @@ def xout(board, row, col):
 
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
-
     Args:
         board (list): The current working chessboard.
         row (int): The current working row.
@@ -115,15 +105,14 @@ def recursive_solve(board, row, queens, solutions):
         solutions.append(get_solution(board))
         return (solutions)
 
-    for c in range(len(board)):
+for c in range(len(board)):
         if board[row][c] == " ":
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
-
-    return (solutions)
+            return (solutions)
 
 
 if __name__ == "__main__":
